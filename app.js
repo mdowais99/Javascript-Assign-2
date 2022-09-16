101e2f9ebafbd51c6c2b3ac8f2b5314d2f0c175d
mdowais99 <mdowais905@gmail.com>
2022-09-16 19:37:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,130 @@
+// var cityCheck = prompt("Name any one big city of Pakistan");
+// var cities = ["Karachi","Lahore","Faisalabad", "hayderabad","Peshawer", "Quetta"];
+// cityCheck = cityCheck.toUpperCase();
+// for(var i = 0; i<=5; i++){
+//     if (cityCheck===cities[i]){
+//         alert("Your city is found in the list");
+//     }
+// }
+
+
+
+
+// var mobileModel= prompt("Your fav mobile phone model is :");
+// alert("Total Characters of your input is: "+ mobileModel.length);
+
+
+// var input = prompt("Enter your name: ");
+// var last = input.charAt(input.length -1);
+// alert("The last character of your input is : "+ last);
+
+
+// var country = "Pakistani";
+// var find = country.indexOf("n");
+// alert ("The index of n is : "+find);
+
+
+// var userName = prompt("Enter User Name : ");
+// var specialChar = !@#%^&*;
+// if if(userName.value.match(specialChar)) {
+//     // var again = prompt("enter your user name again : ");
+//     alert("okay");
+// }
+
+
+// var st = "the quick brown fox jumps over the lazy dog";
+// var count = (st.match(/the/g) || []).length;
+// alert(count);
+
+
+// var country = "Pakistani";
+// var check = country.charAt(3);
+// alert("the character at 3rd index is : "+ check);
+
+
+// var city = "Hyderabad";
+// city = city.replace(/Hyder/g, "Islam");
+// alert(city);
+
+
+// var para = "Ali and Sami are best friends. They play cricket and football together.";
+// para = para.replace(/and/g, "&");
+// alert(para);
+
+
+// var numb = Math.round(prompt("enter any number with decimel : "));
+// alert(numb);
+
+// var numb = Math.floor(prompt("Enter any number in decimel : "));
+// alert(numb);
+
+
+// var numb = Math.ceil(prompt("Enter any number in decimel : "));
+// alert(numb);
+
+
+// var numb = Math.round(prompt("enter any number with decimel in negative ; "));
+// alert(numb);
+
+
+// var numb = Math.floor(prompt("enter any number with decimel in negative ; "));
+// alert(numb);
+
+
+// var numb = Math.ceil(prompt("enter any number with decimel in negative ; "));
+// alert(numb);
+
+
+// var numb = Math.ceil (Math.random()*6);
+// alert(numb);
+
+
+// var numb = Math.ceil(Math.random()*2);
+// if(numb == 1){
+//     alert("Tails");
+// }
+// else{
+//     alert("Heads");
+// }
+
+
+// var numb = Math.ceil(Math.random()*10);
+// var askin = prompt("Enter Number between 1 to 10");
+// if(askin == numb){
+//     alert("Contgrats you guess right");
+// }
+// else{
+//     alert("Better luck next time");
+// }
+
+
+
+// var namE = prompt("Enter your weight : ");
+// alert("Your weigth is: "+namE+" kgs");
+
+
+// var namE = prompt("Enter your weight : ");
+// var age = parseInt(namE)
+// alert("Your weigth is: "+age+" kgs");
+
+
+// var numb = "472";
+// var newNumb = parseInt(numb);
+// document.write(typeof newNumb);
+
+
+// var numb = "472";
+// var newNumb = Number(numb);
+// document.write(typeof newNumb);
+
+
+
+// var numb= 35.36;
+// var newString= numb.toString();
+// alert(typeof newString );
+
+
+// var numb = 25.2525353;
+// var prettyNumn = numb.toFixed(2);
+// alert(prettyNumn);
+
